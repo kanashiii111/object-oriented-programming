20fba202b48250af8e5b4fe7110b36eda3219d42
kanashiii111 <FrageSK@yandex.ru>
2026-04-15 22:59:03 +0300

    lab3 init

---
+++ lab3/src/main/java/org/oop/lab3/Lab3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.oop.lab3;
+
+/**
+ *
+ * @author shizzr
+ */
+public class Lab3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
